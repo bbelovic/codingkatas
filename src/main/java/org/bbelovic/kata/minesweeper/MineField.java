c1932bccb79ed72dae0928aa0f8a7b0ab70e18a7
bbelovic <bbelovic@gmail.com>
2017-06-16 21:34:16 +0200

    MineField internally uses char array instead of String.

--- src/main/java/org/bbelovic/kata/minesweeper/MineField.java
+++ src/main/java/org/bbelovic/kata/minesweeper/MineField.java
@@ -5,37 +5,35 @@ import java.util.List;
 
 public class MineField {
 
-    private String mines;
+    private final char[] mines;
     private int position = -1;
 
     public MineField(String s) {
-        mines = s;
+        mines = s.toCharArray();
     }
 
     public int size() {
-        return mines.length();
+        return mines.length;
     }
 
     public boolean hasMineOnPosition(int position) {
-        char c = mines.charAt(position);
+        char c = mines[position];
         return c == '*';
     }
 
     public boolean canMoveForward() {
-        return (position + 1) <= mines.toCharArray().length - 1;
+        return (position + 1) <= mines.length - 1;
     }
 
     public MineFieldPosition moveForward() {
         position = position + 1;
-        char fieldValue = mines.toCharArray()[position];
-        List<Character> neighbours = new ArrayList<>();
+        final char fieldValue = mines[position];
+        final List<Character> neighbours = new ArrayList<>();
         if (position - 1 >= 0) {
-            // add left neighbour
-            neighbours.add(mines.toCharArray()[position - 1]);
+            neighbours.add(mines[position - 1]);
         }
-        if (position + 1 < mines.toCharArray().length) {
-            // add right
-            neighbours.add(mines.toCharArray()[position + 1]);
+        if (position + 1 < mines.length) {
+            neighbours.add(mines[position + 1]);
         }
         return new MineFieldPosition(fieldValue, neighbours);
     }
