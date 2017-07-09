f8c0fba272a062b7f43e3485f03a24b6a5450720
bbelovic <bbelovic@gmail.com>
2017-07-09 21:42:08 +0200

    Added more tests for mine field loader. Using JUnit test parameters

--- src/test/java/org/bbelovic/kata/minesweeper/MineFieldLoaderShould.java
+++ src/test/java/org/bbelovic/kata/minesweeper/MineFieldLoaderShould.java
@@ -1,23 +1,58 @@
 package org.bbelovic.kata.minesweeper;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.Collection;
 
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class MineFieldLoaderShould {
+
+    private String filename;
+    private MineField expectedMinefield;
+    private final MineFieldLoader loader = new MineFieldLoader();
+
+    public MineFieldLoaderShould(String filename, MineField expectedMinefield) {
+        this.filename = filename;
+        this.expectedMinefield = expectedMinefield;
+    }
+
+    @Parameters(name = "{index}: filename: {0}, expected minefield: {1}")
+    public static Collection<Object[]> testData() {
+        return asList(new Object[][] {
+                {"test22.txt",new MineField(2, 2, asList('.', '.', '.', '.'))},
+                {"test00.txt",new MineField(0, 0, emptyList())},
+                {"test11.txt",new MineField(1, 1, singletonList('.'))},
+                {"test33.txt",new MineField(3, 3, asList('.','.','.','.','.','*','.','.','.'))},
+                {"test14.txt",new MineField(1, 4, asList('.','.','.','.'))}
+        });
+    }
+
     @Test
     public void
     load_data_from_file_into_string() {
-        URL resource = Thread.currentThread().getContextClassLoader().getResource("test.txt");
-        Path path = Paths.get(resource.getFile());
-        MineFieldLoader loader = new MineFieldLoader();
+        Path path = getPathtoFile();
         MineField actual = loader.load(path);
-        MineField expected = new MineField(2, 2, Arrays.asList('.', '.', '.', '.'));
-        Assert.assertEquals(expected, actual);
+        assertEquals(expectedMinefield, actual);
+    }
+
+    private Path getPathtoFile() {
+        URL resource = Thread.currentThread().getContextClassLoader().getResource(filename);
+        if (resource == null) {
+            throw new IllegalStateException(format("Unable to create path for input file [%s]", filename));
+        }
+        return Paths.get(resource.getFile());
     }
 
 }
--- src/test/java/org/bbelovic/kata/minesweeper/MineFieldShould.java
+++ src/test/java/org/bbelovic/kata/minesweeper/MineFieldShould.java
@@ -1,6 +1,9 @@
 package org.bbelovic.kata.minesweeper;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +11,7 @@ import java.util.List;
 import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class MineFieldShould {
 
     @Test(expected = NullPointerException.class)
@@ -34,28 +38,42 @@ public class MineFieldShould {
         new MineField(2, 2, emptyList());
     }
 
+    private int rows;
+    private int cols;
+    private String mineFieldPositions;
+    private String expectedOutput;
+
+    public MineFieldShould(int rows, int cols, String mineFieldPositions, String expectedOutput) {
+        this.rows = rows;
+        this.cols = cols;
+        this.mineFieldPositions = mineFieldPositions;
+        this.expectedOutput = expectedOutput;
+    }
+
     @Test
     public void
     produce_swept_output_for_correct_input() {
-        Object [][] testData = new Object [][] {
-                {3, 3, ".....*...", "011\n01*\n011"},
-                {4, 4, "*........*......", "*100\n2210\n1*10\n1110"},
-                {3, 5, "**.........*...", "**100\n33200\n1*100"},
-                {1, 4, "*...", "*100"},
-                {1, 4, ".*..", "1*10"},
-                {1, 4, "*.*.", "*2*1"},
-                {1, 4, "....", "0000"},
-                {1, 4, "****", "****"},
-                {1, 1, "*", "*"},
-                {0, 0, "", ""},
-                {2, 2,"....", "00\n00"}
-        };        for (final Object[] each: testData) {
-            List<Character> fields = getMineFieldPositionsCollection(each[2]);
-            MineField mineField = new MineField((Integer) each[0], (Integer) each[1], fields);
-            SweptMineField actual = mineField.sweep();
-            assertEquals("Failed to process: "+ each[0], each[3], actual.asText());
-        }
+        final List<Character> fields = getMineFieldPositionsCollection(mineFieldPositions);
+        final MineField mineField = new MineField(rows, cols, fields);
+        final SweptMineField actual = mineField.sweep();
+        assertEquals(expectedOutput, actual.asText());
+    }
 
+    @Parameters(name = "{index}: MineField({0}, {1}, {2}).sweep() == {3}")
+    public static Object[][] testData() {
+        return new Object [][] {
+                    {3, 3, ".....*...", "011\n01*\n011"},
+                    {4, 4, "*........*......", "*100\n2210\n1*10\n1110"},
+                    {3, 5, "**.........*...", "**100\n33200\n1*100"},
+                    {1, 4, "*...", "*100"},
+                    {1, 4, ".*..", "1*10"},
+                    {1, 4, "*.*.", "*2*1"},
+                    {1, 4, "....", "0000"},
+                    {1, 4, "****", "****"},
+                    {1, 1, "*", "*"},
+                    {0, 0, "", ""},
+                    {2, 2,"....", "00\n00"}
+            };
     }
 
     private List<Character> getMineFieldPositionsCollection(Object each) {
