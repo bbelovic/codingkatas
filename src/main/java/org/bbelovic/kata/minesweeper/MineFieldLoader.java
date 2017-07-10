4a6d7498b13a1674578ad1f08240ed093fcb0918
bbelovic <bbelovic@gmail.com>
2017-07-10 22:50:59 +0200

    Little polishing. Renamed variable and unified declarations

--- src/main/java/org/bbelovic/kata/minesweeper/MineFieldLoader.java
+++ src/main/java/org/bbelovic/kata/minesweeper/MineFieldLoader.java
@@ -6,12 +6,13 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.String.format;
 import static java.util.Collections.emptyList;
 
 class MineFieldLoader {
     MineField load(final Path path) {
-        final List<String> fileContent = readAllLines(path);
-        return parseFromLines(fileContent);
+        final List<String> lines = readAllLines(path);
+        return parseFromLines(lines);
     }
 
     private List<String> readAllLines(final Path path) {
@@ -19,17 +20,21 @@ class MineFieldLoader {
         try {
             lines = Files.readAllLines(path);
         } catch (final IOException e) {
+            System.err.println(format("Unable to read all lines from file [%s]", path));
             e.printStackTrace();
         }
         return lines;
     }
 
     private MineField parseFromLines(final List<String> fileLines) {
+            int rows = 0;
+            int cols = 0;
+            List<Character> characters = emptyList();
             if (fileLines.size() > 1) {
                 String[] dimensions = fileLines.get(0).split(" ");
-                int rows = Integer.parseInt(dimensions[0]);
-                int cols = Integer.parseInt(dimensions[1]);
-                List<Character> characters = new ArrayList<>();
+                rows = Integer.parseInt(dimensions[0]);
+                cols = Integer.parseInt(dimensions[1]);
+                characters = new ArrayList<>();
                 for (int i = 1; i < fileLines.size(); i++) {
                     char[] chars = fileLines.get(i).toCharArray();
                     for (char each: chars) {
@@ -38,7 +43,7 @@ class MineFieldLoader {
                 }
                 return new MineField(rows, cols, characters);
             } else {
-                return new MineField(0, 0, emptyList());
+                return new MineField(rows, cols, characters);
             }
     }
 }
