ca93ebe0f5a6db1ed0de66f22a0f1c9f59d67fee
bbelovic <bbelovic@gmail.com>
2020-02-13 22:21:38 +0100

    Progressive tax calculator - first test passing

--- src/main/java/org/bbelovic/kata/ProgressiveTaxCalculator.java
+++ src/main/java/org/bbelovic/kata/ProgressiveTaxCalculator.java
@@ -1,4 +1,28 @@
 package org.bbelovic.kata;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 public class ProgressiveTaxCalculator {
+    private final Map<Long, BigDecimal> taxBrackets;
+
+    public ProgressiveTaxCalculator(Map<Long, BigDecimal> taxBrackets) {
+        this.taxBrackets = taxBrackets;
+    }
+
+    public BigDecimal calculate(BigDecimal income) {
+        BigDecimal tax = BigDecimal.ZERO;
+        int lowBound = 0;
+        for (var entry: taxBrackets.entrySet()) {
+            var bracket = entry.getKey() - lowBound;
+            if (income.compareTo(BigDecimal.valueOf(bracket)) > 0) {
+                tax = tax.add(BigDecimal.valueOf(bracket).multiply(entry.getValue()));
+            } else {
+                tax = tax.add(income.multiply(entry.getValue()));
+                break;
+            }
+            income = income.subtract(BigDecimal.valueOf(bracket));
+        }
+        return tax;
+    }
 }
--- src/test/java/org/bbelovic/kata/ProgressiveTaxCalculatorTest.java
+++ src/test/java/org/bbelovic/kata/ProgressiveTaxCalculatorTest.java
@@ -3,13 +3,21 @@ package org.bbelovic.kata;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProgressiveTaxCalculatorTest {
     @Test
     public void shouldComputeProgressiveTaxFromGivenSalary() {
-        Map.of(10_000, BigDecimal.ONE.add(BigDecimal.TEN).divide(BigDecimal.TEN));
-        Map.of(10_000, new BigDecimal("0.11"), 38_000, new BigDecimal("0.20"));
-        ProgressiveTaxCalculator calculator = new ProgressiveTaxCalculator();
+        Map<Long, BigDecimal> m = new LinkedHashMap<>();
+        m.put(10_000L, new BigDecimal("0.11"));
+        m.put(38_000L, new BigDecimal("0.20"));
+        ProgressiveTaxCalculator calculator = new ProgressiveTaxCalculator(m);
+        var income = new BigDecimal("12500");
+        var actual = calculator.calculate(income);
+        assertThat(actual).
+                isEqualTo(new BigDecimal("1600.00"));
     }
 }
